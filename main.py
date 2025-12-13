bd7606ca03254b056789cbb29f99f9b0faf0c781
AviTech007 <Avi20074@gmail.com>
2025-12-13 07:33:05 +0530

    corr

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ CORS(app, origins=[
 ])
 
 # Get API keys from environment variables
-GROQ_API_KEY = os.getenv('GROQ_API_KEY') Howdy! 
+GROQ_API_KEY = os.getenv('GROQ_API_KEY') 
 NEWS_API_KEY = os.getenv('NEWS_API_KEY')
 
 @app.route('/')
